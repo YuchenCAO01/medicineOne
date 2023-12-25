e931e60517e7f9b51f386897debe31c935c558d7
YuchenCAO01 <clark1320681142@gmail.com>
2023-12-25 14:20:06 +0800

    reverse

--- medicine-one-backend/src/main/java/Medione/config/WebConfig.java
+++ medicine-one-backend/src/main/java/Medione/config/WebConfig.java
@@ -32,7 +32,11 @@ public class WebConfig implements WebMvcConfigurer {
             // allow path
             registry.addMapping("/**")
 //                    .allowedOrigins("http://localhost:8080/")   //local server
-                    .allowedOrigins("http://localhost:80/", "https://182.92.169.27:80/", "https://182.92.169.27/medione-app/")     //developing website
+                    .allowedOrigins("http://localhost:80/",
+                                    "https://182.92.169.27:80/",
+                                    "https://182.92.169.27/medione-app/",
+                                    "http://182.92.169.27:80/",
+                                    "http://182.92.169.27/medione-app/")     //developing website
 //                    .allowedOrigins("*")     //release website
                     .allowCredentials(true)
                     .allowedMethods("*")
