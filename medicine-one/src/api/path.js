a8ce6c59a0093455eb25593537fbd4a6382cef58
YuchenCAO01 <clark1320681142@gmail.com>
2023-12-25 11:24:17 +0800

    Update path.js

--- medicine-one/src/api/path.js
+++ medicine-one/src/api/path.js
@@ -1,6 +1,6 @@
 // Encapsulation of back end API request path
 const base = {
-  baseUrl       : 'http://182.92.169.27/medione-api:',
+  baseUrl       : 'http://182.92.169.27/medione-api',
   // baseUrl       : "https://medione.herokuapp.com",
   //baseUrl       : "https://medione-dev.herokuapp.com",
   login         : '/user/login',
